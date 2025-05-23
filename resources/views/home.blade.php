84cb7b52a74341e6346450a50b532c3f868f7aa4
GiuntaGiovanni <jhonniest741@gmail.com>
2025-05-23 11:50:42 +0200

    feat: enhance home route to pass routes collection for display in home view

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,35 @@
+@php
+$allUri = [];
+    foreach ($routes as $route) {
+      foreach ($route as $uri) {
+         $methods = implode(', ', $route->methods());
+           if ($methods === "GET, HEAD") {   
+            if (is_string($uri)) {
+             array_push($allUri, $uri);
+            }
+         }
+        
+      }
+   }
+@endphp
+
+
 @extends('layouts.app')
 
 @section('content')
-   <h1>questa è la home</h1>
+<div class="container py-5 d-flex justify-content-center">
+   <div style="width: 300px;"> 
+      <h1 class="mb-4 text-center text-primary">Questa è la Home</h1>
+
+      <ul class="list-group shadow text-center">
+         @foreach ($allUri as $uri)
+            <li class="list-group-item">
+               <a href="{{ $uri }}" class="text-decoration-none text-dark d-block">
+                  {{ $uri }}
+               </a>
+            </li>
+         @endforeach
+      </ul>
+   </div>
+</div>
 @endsection
