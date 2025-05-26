dbcee06ecced7841e7fcb1afd3a01e7d20a4b8fc
GiuntaGiovanni <jhonniest741@gmail.com>
2025-05-27 00:57:21 +0200

    feat: add project creation form to the projects view

---
+++ resources/views/projects/create.blade.php
@@ -0,0 +1,56 @@
+@extends('layouts.projects')
+
+@section('title', "Aggiungi un projects")
+
+@section('content')
+
+<form action="{{ route('projects.store') }}" method="POST" class="container mt-4 p-4 border rounded shadow-sm bg-light">
+    {{-- token di protezione che identifica che la chiamata post avvenga tramite un form del sito stesso --}}
+    @csrf
+
+    {{-- titolo --}}
+    <div class="mb-3">
+        <label for="title" class="form-label">Titolo</label>
+        <input type="text" name="title" id="title" class="form-control" required>
+    </div>
+
+    {{-- cliente --}}
+    <div class="mb-3">
+        <label for="client" class="form-label">Cliente</label>
+        <input type="text" name="client" id="client" class="form-control" required>
+    </div>
+
+    {{-- start date --}}
+    <div class="mb-3">
+        <label for="start_date" class="form-label">Data Inizio</label>
+        <input type="date" name="start_date" id="start_date" class="form-control">
+    </div>
+
+    {{-- end date --}}
+    <div class="mb-3">
+        <label for="end_date" class="form-label">Data Fine</label>
+        <input type="date" name="end_date" id="end_date" class="form-control">
+    </div>
+
+    {{-- stato progetto --}}
+    <div class="mb-3">
+        <label for="state" class="form-label">Stato Progetto</label>
+        <select id="state" name="state" class="form-select">
+            <option value="completed">Completato</option>
+            <option value="in_progress">In Corso</option>
+        </select>
+    </div>
+
+    {{-- descrizione --}}
+    <div class="mb-3">
+        <label for="description" class="form-label">Descrizione</label>
+        <textarea name="description" id="description" class="form-control" rows="5"></textarea> 
+    </div>
+
+    {{-- pulsante per inviare il progetto --}}
+    <div class="text-end">
+        <input type="submit" value="Salva" class="btn btn-primary"></button>
+    </div>
+</form>
+
+@endsection
