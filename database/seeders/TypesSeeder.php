e6897eebe3664235835cce4665bd21e0fd4f0170
GiuntaGiovanni <jhonniest741@gmail.com>
2025-05-27 17:58:52 +0200

    feat: add TypesSeeder to populate type data in the database

---
+++ database/seeders/TypesSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Type;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+use Faker\Generator as Faker;
+
+class TypesSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(Faker $faker): void
+    {
+        $types = ['javascript', 'php', 'python', 'java'];
+
+        foreach ($types as $type) {
+            $newType = new Type();
+
+            $newType->name = $type;
+            $newType->description = $faker->sentence();
+
+            $newType->save();
+        }
+    }
+}
