1ecef1ac875c34b0798da4e6a448beb7442ee89c
GiuntaGiovanni <jhonniest741@gmail.com>
2025-06-02 17:24:27 +0200

    feat: implement user seeder and update user model for email verification, disabled manual registration

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -25,7 +25,8 @@ class DatabaseSeeder extends Seeder
         $this->call([
             TypesSeeder::class,
             ProjectsSeeder::class,
-            TechnologiesSeeder::class
+            TechnologiesSeeder::class,
+            UserSeeder::class
         ]);
     }
 }
